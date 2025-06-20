cbe97b50ba0339b3c7b78095dfa2ce4172a4fbb5
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-06-20 17:55:09 +0700

    ddd

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,12 +17,22 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      // Cảnh báo & tự động xóa import thừa:
+      'unused-imports/no-unused-imports': 'error',
+      // (Tùy chọn) Tự cảnh báo biến không dùng:
+      'no-unused-vars': 'warn',
+      // (Tùy chọn) Không cảnh báo nếu biến bắt đầu bằng '_'
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
       ],
     },
   },
